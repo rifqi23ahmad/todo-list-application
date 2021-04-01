97facb4d6292cdf099c0898fe3eb75c82aa6accd
Ah Rifqi Ubaidilah <42555735+rifqi23ahmad@users.noreply.github.com>
2021-04-02 02:45:43 +0700

    update Todo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import TodoForm from './Components/TodoForm'
+import TodoList from './Components/TodoList'
 
 function App() {
   return (
     <div className="TodoList">
-     <TodoForm/>
+    
+     <TodoList/>
     </div>
   );
 }
---
+++ src/Components/TodoList.jsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import TodoForm from './TodoForm';
+
+function TodoList() {
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = todo => {
+        if (!todo.text || /^\s*$/.test(todo.text)){
+            return;
+        }
+
+        const newTodos =[todo, ...todos];
+
+        setTodos(newTodos);
+        console.log(todo, ...todos);
+    }
+
+    return (
+        <div>
+            <h1>Mau Ngapain Hari ini?</h1>
+            <TodoForm onSubmit={addTodo} />
+
+        </div>
+    );
+}
+
+export default TodoList;
