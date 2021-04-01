5f317d7e6bf2ef73549d29d5eba48828a4362936
Ah Rifqi Ubaidilah <42555735+rifqi23ahmad@users.noreply.github.com>
2021-04-02 01:41:13 +0700

    on progress

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TodoForm from './Components/TodoForm'
+
+function App() {
+  return (
+    <div className="TodoList">
+     <TodoForm/>
+    </div>
+  );
+}
+
+export default App;
