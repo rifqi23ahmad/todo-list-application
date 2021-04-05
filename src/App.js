a914cda44dcf8faad5ffb5ab020d6700f6ac8e4e
Ah Rifqi Ubaidilah <42555735+rifqi23ahmad@users.noreply.github.com>
2021-04-06 00:51:21 +0700

    masih mencoba menghubungkan halaman

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import './App.css';
-import TodoList from './Components/TodoList';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+
+import Home from "./Components/Home";
+import UpdateProfile from "./Components/FormUpdateProfile";
+// import Register from "./Components/FormRegister";
+// import Login from "./Components/FormLogin";
+// import Profil from "./Components/Profile";
+// import Password from "./Components/FormUpdatePass"
+
 
 function App() {
   return (
-    <div className='todo-app'>
-      <TodoList />
-    </div>
+  <div>
+    <Router>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <link to="/FormUpdateProfile">UpdateProfile</link>
+            </li>
+          </ul>
+        </nav>
+        <div>
+          <Switch>
+            <Route path="/Home">
+              <Home/>
+            </Route>
+            <Route path="/FormUpdateProfile">
+              <UpdateProfile/>
+            </Route>
+          </Switch>
+        </div>
+      </header>
+    </Router>
+  </div>
   );
 }
 
